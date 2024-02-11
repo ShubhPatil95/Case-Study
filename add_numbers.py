4aed405acaf0977a051ca74840db9dd7a7530a13
shubhamrpatil122@gmail.com <shubhamrpatil122@gmail.com>
2024-02-11 15:51:21 +0530

    third commit

--- add_numbers.py
+++ add_numbers.py
@@ -6,5 +6,8 @@ if __name__ == "__main__":
     #num1 = int(input("Enter the first number: "))
     #num2 = int(input("Enter the second number: "))
     #result = add_numbers(num1, num2)
-    print("The result is:")
+    import time 
+    for i in range(100):
+        time.sleep(5)
+        print("The result is:",i)
 
