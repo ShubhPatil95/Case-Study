9f66282eff8bb50ba1e8711646f829f88985af4e
shubhamrpatil122@gmail.com <shubhamrpatil122@gmail.com>
2024-02-11 18:57:57 +0530

    test2

--- add_numbers.py
+++ add_numbers.py
@@ -1,6 +1,10 @@
-import os
+import json
 
-# Read values from environment variables
-a = int(os.getenv('A', '10'))  # Default value is 10 if not found in ConfigMap
-b = int(os.getenv('B', '20'))  # Default value is 20 if not found in ConfigMap
+# Read values from config.json
+with open('config.json') as f:
+    config = json.load(f)
+
+a = config.get('A', 10)  # Default value is 10 if 'A' key is not found
+b = config.get('B', 20)  # Default value is 20 if 'B' key is not found
+
 print(a + b)
