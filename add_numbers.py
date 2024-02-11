9241d742db0fabc4d39badd116dfce389cf56516
shubhamrpatil122@gmail.com <shubhamrpatil122@gmail.com>
2024-02-11 18:51:57 +0530

    test1

--- add_numbers.py
+++ add_numbers.py
@@ -1,10 +1,7 @@
 import os
-import time
 
-for i in range(10):
-    # Read values from environment variables
-    a = int(os.getenv('A', '10'))  # Default value is 10 if not found in ConfigMap
-    b = int(os.getenv('B', '20'))  # Default value is 20 if not found in ConfigMap
-
-    print(a + b)
-    time.sleep(1)
+# Read values from environment variables
+a = int(os.getenv('A', '10'))  # Default value is 10 if not found in ConfigMap
+b = int(os.getenv('B', '20'))  # Default value is 20 if not found in ConfigMap
+print(a + b)
+time.sleep(1)
