74805069440b45ee85e24631acc303aa3659a5f0
shubhamrpatil122@gmail.com <shubhamrpatil122@gmail.com>
2024-02-11 22:40:13 +0530

    sixth commit

--- add_numbers.py
+++ add_numbers.py
@@ -1,10 +1,3 @@
-import json
-
-# Read values from config.json
-with open('/config/config.json') as f:
-    config = json.load(f)
-
-a = config.get('A', 10)  # Default value is 10 if 'A' key is not found
-b = config.get('B', 20)  # Default value is 20 if 'B' key is not found
-
-print(a + b)
+a=10
+b = 20
+print(a+b)
