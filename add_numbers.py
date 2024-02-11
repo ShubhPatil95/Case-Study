58022897eccd4fa77d9c2b37ff6b0361e21e2ac9
shubhamrpatil122@gmail.com <shubhamrpatil122@gmail.com>
2024-02-11 15:36:48 +0530

    updated add_numbers

--- add_numbers.py
+++ add_numbers.py
@@ -3,8 +3,8 @@ def add_numbers(num1, num2):
     return num1 + num2
 
 if __name__ == "__main__":
-    num1 = int(input("Enter the first number: "))
-    num2 = int(input("Enter the second number: "))
-    result = add_numbers(num1, num2)
-    print("The result is:", result)
+    #num1 = int(input("Enter the first number: "))
+    #num2 = int(input("Enter the second number: "))
+    #result = add_numbers(num1, num2)
+    print("The result is:")
 
