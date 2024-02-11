4fb3d2879dd59b4b2727eab8e5c00ecedf60a846
shubhamrpatil122@gmail.com <shubhamrpatil122@gmail.com>
2024-02-11 18:14:58 +0530

    firth

--- add_numbers.py
+++ add_numbers.py
@@ -1,2 +1,7 @@
-print("This is just for testing")
-print("application  is  started")
+import time
+
+for i in range(100):
+    print("This is just for testing")
+    print("application  is  started")
+    time.sleep(5)
+
